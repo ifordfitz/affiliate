162cab00815b78f08f4d3252b380992fc4fc6132
Ian Fitzgerald <ifordfitz@gmail.com>
2019-05-08 14:50:04 -0400

    now in columns with some padding

--- models/brands.js
+++ models/brands.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const brandSchema = new mongoose.Schema({
   name: {type: String, required: true},
+  website: String,
   image: {type: String, required: true},
   ownItem: Boolean,
   socialMedia: Boolean,
